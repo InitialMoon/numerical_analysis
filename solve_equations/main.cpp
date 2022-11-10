c437d9fa9d9e812c59c6d628f621b5f0f80a0810
InitialDream <101309167+InitialMoon@users.noreply.github.com>
2022-11-10 10:42:26 +0800

    高斯消元法完毕

--- solve_equations/main.cpp
+++ solve_equations/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #define MAXSIZE 100
 using namespace std;
 
@@ -10,20 +11,30 @@ double Z[MAXSIZE]; // 化为上三角矩阵后的系数向量
 double X[MAXSIZE]; // 解的存放单元
 int n = 0;
 int methodType = 0;
+double(*Lii)(int) = NULL;
 
 void input();
 void select();
 void back();
+void solve();
+void Eliminate(); // 消元过程
+void output();
+double GuessLii(int i);
+double CroutLii(int i);
 
 int main() {
+	while (methodType <= 0 || methodType > 4) { // 选项合法性检测
+		select();
+	}
 	input();
-	select();
+	solve();
+	output();
 }
 
 void input() {
-	cout << "其输入方程组的秩";
+	cout << "其输入方程组的秩:";
 	cin >> n;
-	cout << "请输入方程组的增广矩阵" << endl;
+	cout << "请输入方程组的增广矩阵:" << endl;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			cin >> A[i][j];
@@ -32,16 +43,80 @@ void input() {
 	}
 }
 
+void output() {
+	for (int i = 0; i < n; i++) {
+		cout << "x" << i + 1 << " = " << X[i] << endl;
+	}
+}
+
 void select() {
 	cout << "请选择你想使用的方法" << endl;
 	cout << "1.高斯消元法" << endl;
 	cout << "2.克劳特消元法" << endl;
-	cout << "3.平方根法" << endl;
-	cout << "4.列主元素法" << endl;
 	cin >> methodType;
+	switch (methodType) {
+	case 1:
+		Lii = GuessLii;
+		break;
+	case 2:
+		Lii = CroutLii;
+		break;
+	default:
+		break;
+	}
+}
+
+void solve() {
+	Eliminate();
+	back();
 }
 
 // 回带上三角矩阵过程
 void back() {
+	for (int i = n - 1; i >= 0; i--) {
+		X[i] = Z[i];
+		for (int k = i + 1; k < n; k++) {
+			X[i] -= U[i][k] * X[k];
+		}
+		X[i] /= U[i][i];
+	}
+}
 
+// 从上至下消元过程
+void Eliminate() {
+	for (int i = 0; i < n; i++) {
+		// Lij计算
+		Lii(i);
+		for (int j = 0; j < i; j++) {
+			L[i][j] = A[i][j];
+			for (int k = 0; k < j; k++) {
+				L[i][j] -= L[i][k] * U[k][j];
+			}
+			L[i][j] /= U[j][j];
+		}
+
+		// Uij计算
+		for (int j = i; j < n; j++) {
+			U[i][j] = A[i][j];
+			for (int k = 0; k < i; k++) {
+				U[i][j] -= L[i][k] * U[k][j];
+			}
+		}
+
+		// Zi计算
+		Z[i] = B[i];
+		for (int k = 0; k < i; k++) {
+			Z[i] -= L[i][k] * Z[k];
+		}
+	}
+}
+
+// 高斯消元法的lii计算方式
+double GuessLii(int i) {
+	L[i][i] = 1;
+	return 1;
+}
+
+double CroutLii(int i) {
+	return 0.0;
 }
